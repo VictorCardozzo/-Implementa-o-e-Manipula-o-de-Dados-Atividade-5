92f8b98a45005a85cdaf1944f8ca0bfd62bed907
Victor Cardozo <cardozovictor1234567@gmail.com>
2025-11-30 18:11:19 -0300

    Add SQL commands to delete specific records

--- deletes.sql
+++ deletes.sql
@@ -1,1 +1,11 @@
-DELETE ...
+-- deletes.sql
+USE gymfit;
+
+-- 1) Remover matrícula específica (condição)
+DELETE FROM MATRICULA WHERE MatriculaID = 2;
+
+-- 2) Remover pagamento com ID específico
+DELETE FROM PAGAMENTO WHERE PagamentoID = 2;
+
+-- 3) Remover equipamento que está obsoleto (exemplo)
+DELETE FROM EQUIPAMENTO WHERE EquipID = 2;
