79a26a31e3ebb1f6521f0f8caa61de4ca1ea05e8
Victor Cardozo <cardozovictor1234567@gmail.com>
2025-11-30 18:10:53 -0300

    Add SQL updates for member phone and equipment status

--- updates.sql
+++ updates.sql
@@ -1,1 +1,11 @@
-UPDATE ...
+-- updates.sql
+USE gymfit;
+
+-- 1) Atualizar telefone de um membro
+UPDATE MEMBRO SET Telefone = '11990001111' WHERE MembroID = 1;
+
+-- 2) Trocar status de equipamento
+UPDATE EQUIPAMENTO SET Status = 'Ativo' WHERE EquipID = 2;
+
+-- 3) Ajustar valor de pagamento (exemplo de correção)
+UPDATE PAGAMENTO SET Valor = 130.00 WHERE PagamentoID = 1;
